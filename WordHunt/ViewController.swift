6ea28455e10519b1bda4343d4a278868ccb6cbd9
Anderson <112644138+iskowan@users.noreply.github.com>
2023-05-11 11:06:51 -0400

    Commit 2

--- WordHunt/ViewController.swift
+++ WordHunt/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var letterButtons: UIButton!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        sender.isEnabled = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var listOfLetters = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    // chose 9 letters from this list and then only allow those letters to be used on the keyboard
+    
+    
+    let timeAllowed = 60
+    
 }
 
