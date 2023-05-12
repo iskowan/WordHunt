fe0aef77f51780ef3c26ece48ee8c2eb8dae9a92
Anderson <112644138+iskowan@users.noreply.github.com>
2023-05-12 14:07:05 -0400

    Commit 3

--- WordHunt/ViewController.swift
+++ WordHunt/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var letterButtons: UIButton!
     
+    @IBOutlet var button1: UIButton!
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
         sender.isEnabled = true
@@ -22,9 +23,25 @@ class ViewController: UIViewController {
     }
     
     var listOfLetters = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    
+//    func pickLetters(){
+//        let button1.title = listOfLetters.randomElement()
+//    }
+//
     
     // chose 9 letters from this list and then only allow those letters to be used on the keyboard
+   
     
+    //https://stackoverflow.com/questions/32603825/swift-open-built-in-ios-dictionary-to-find-word-meaning
+//    func wordIsReal(word: String) -> Bool {
+//        let checker = UITextChecker()
+//        let range = NSMakeRange(0, count(word))
+//        let misspelledRange = checker.rangeOfMisspelledWordInString(word, range: range, startingAt: 0, wrap: false, language: "en")
+//
+//        return misspelledRange.location == NSNotFound
+//    }
+   
     
     let timeAllowed = 60
     
